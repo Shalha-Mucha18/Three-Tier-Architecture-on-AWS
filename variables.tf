236a4bfcc6eb58c801518a8fa5586781089041d8
Shalha-Mucha18 <ms.mucha18@gmail.com>
2026-07-06 01:54:02 +0900

    Add variables for AWS configuration and subnet settings in Terraform

---
+++ variables.tf
@@ -0,0 +1,88 @@
+# AWS Configuration
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "profile" {
+  description = "AWS profile"
+  type        = string
+  default     = "default"
+}
+
+# VPC Configuration
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+# Subnet Configuration
+variable "public_subnet1_cidr_block" {
+  description = "CIDR block for the first public subnet"
+  type        = string
+
+}
+
+variable "public_subnet2_cidr_block" {
+  description = "CIDR block for the second public subnet"
+  type        = string
+
+}
+
+variable "private_subnet1_cidr_block" {
+  description = "CIDR block for the first private subnet"
+  type        = string
+
+}
+
+variable "private_subnet2_cidr_block" {
+  description = "CIDR block for the second private subnet"
+  type        = string
+
+}
+
+# Availability Zones
+variable "public_subnet1_az" {
+  description = "Availability zone for the first public subnet"
+  type        = string
+  default     = "us-east-1a"
+}
+
+variable "public_subnet2_az" {
+  description = "Availability zone for the second public subnet"
+  type        = string
+  default     = "us-east-1b"
+}
+
+variable "private_subnet1_az" {
+  description = "Availability zone for the first private subnet"
+  type        = string
+  default     = "us-east-1a"
+}
+
+variable "private_subnet2_az" {
+  description = "Availability zone for the second private subnet"
+  type        = string
+  default     = "us-east-1b"
+}
+
+variable "instance_type" {
+  type = string
+  default = "t2.micro"
+}
+
+# IAM Role Configuration
+variable "instance_profile_role_name" {
+  description = "IAM instance profile role name"
+  type        = string
+  default     = "EC2_SSM"
+}
+
+
+# Target Group configuration
+variable dereg-delay {
+  type = string
+  default     = "10"
+}
